72a89a06b584b921a2dc0286aa75ddfbf4549836
abhin <abhinavk@metropolia.fi>
2022-11-09 19:33:18 +0200

    finished exercise 5 but the output does not appear in the body

---
+++ Module 1. Interactive programs + Conditional expressions and loops/5.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const year = parseInt(prompt('Enter a year.'))
+
+if (year >= 100) {
+    if (year % 4 === 0 && year % 400 === 0) {
+        document.querySelector('#paragraph1').innerHTML = `The year ${year} is a leap year.`
+    }
+}
+
+else if (year < 100) {
+    if (year % 4 === 0) {
+        document.querySelector('#paragraph1').innerHTML = `The year ${year} is a leap year.`
+    }
+}
+
+else {
+    document.querySelector('#paragraph1').innerHTML = `The year ${year} is not a leap year.`
+}
