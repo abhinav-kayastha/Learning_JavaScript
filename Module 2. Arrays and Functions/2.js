a92a34ad2e0c9a78a95c4025b3af5e5eb078ea1f
abhin <abhinavk@metropolia.fi>
2022-11-14 15:50:39 +0200

    finished 2.js and 2.html

--- Module 2. Arrays and Functions/2.js
+++ Module 2. Arrays and Functions/2.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let participants = [];
+const numOfParticipants = parseInt(prompt('How many participants?'));
+let i = 0;
+
+while (i < numOfParticipants) {
+    const name = prompt('Enter name.');
+    participants.push(name);
+    i++;
+}
+
+participants.sort;
+
+for (i of participants) {
+    document.getElementById("list").innerHTML += `<li>${i}</li>`;
+}
+
