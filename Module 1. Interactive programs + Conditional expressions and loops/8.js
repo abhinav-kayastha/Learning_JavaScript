77f991ea831f48f66db09cab4996aceba26f1117
abhin <abhinavk@metropolia.fi>
2022-11-14 15:07:01 +0200

    bugs on module 1, 8.js

--- Module 1. Interactive programs + Conditional expressions and loops/8.js
+++ Module 1. Interactive programs + Conditional expressions and loops/8.js
@@ -2,20 +2,24 @@
 
 let startYear = parseInt(prompt('Enter a starting year.'))
 const endYear = parseInt(prompt('Enter an ending year.'))
+let leapYear = "<ol>"
 
 while (startYear <= endYear) {
     if (startYear % 100 === 0) {
-        if (startYear % 4 === 0 && i % 400 === 0) {
-            document.querySelector('#list1').innerHTML = `<li>{startYear}</li>`;
+        if (startYear % 4 === 0 && startYear % 400 === 0) {
+            leapYear += `<li>${startYear}</li>`;
             startYear++
         }
     }
 
     else if (startYear % 4 === 0) {
-    document.querySelector('#list1').innerHTML = `<li>{startYear}</li>`;
-    startYear++
+        leapYear += `<li>${startYear}</li>`;
+        startYear++
     }
     else {
         startYear++
     }
 }
+
+leapYear += '</ol>'
+document.querySelector('#list1').innerHTML = leapYear
